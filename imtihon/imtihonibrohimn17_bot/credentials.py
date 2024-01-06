0ca1846c582e5cb4518068e1a1cbe9e6403ec114
andcr <fatxullohibrohim@gmail.com>
2024-01-06 18:11:39 +0500

    first com

---
+++ imtihon/imtihonibrohimn17_bot/credentials.py
@@ -0,0 +1,3 @@
+# Do not remove these 2 lines:
+BOT_TOKEN = '5754196545:AAH-iJtTHHFhdq5BnVqKhVPSzbx_H9zVa5o'  # You should consider using env variables or a secret manager for this.
+APP_NAME = 'imtihonibrohimn17_bot'
